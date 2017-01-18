30914038468e76e78d959fc9c0060e4d1ff0282c
kpm25 <kieran928@gmail.com>
2017-01-19 03:33:42 +1300

    .....

--- bin/import.sql
+++ bin/import.sql
@@ -4,9 +4,15 @@ insert into role (name) values ('ROLE_USER');
 -- Insert two users (passwords are both 'password')
 insert into user (username,enabled,password,role_id) values ('user',true,'$2a$08$wgwoMKfYl5AUE9QtP4OjheNkkSDoqDmFGjjPE2XTPLDe9xso/hy7u',1);
 insert into user (username,enabled,password,role_id) values ('user2',true,'$2a$08$wgwoMKfYl5AUE9QtP4OjheNkkSDoqDmFGjjPE2XTPLDe9xso/hy7u',1);
+insert into user (username,enabled,password,role_id) values ('kpm25',true,'$2a$06$wu2iwXLwCJq4NmAF5AFRi.UgaJ.Ui8zaSWzPTbaLDBI5il3rPOSl2',1);
+
+
 
 -- Insert tasks
 insert into task (complete,description, user_id) values (true,'Code Task entity',1);
 insert into task (complete,description, user_id) values (false,'Discuss users and roles',1);
 insert into task (complete,description, user_id) values (false,'Enable Spring Security',2);
 insert into task (complete,description, user_id) values (false,'Test application',2);
+insert into task (complete,description, user_id) values (true,'Kierans Security',3);
+insert into task (complete,description, user_id) values (false,'Kierans application',3);
+
